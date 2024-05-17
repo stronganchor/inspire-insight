9334270b371fe0d2305d32ac496b34ee9f4fa002
stronganchor <77190959+stronganchor@users.noreply.github.com>
2024-05-17 12:19:42 +0300

    Update tis-shortcode.php

--- ticker-insight-score/includes/tis-shortcode.php
+++ ticker-insight-score/includes/tis-shortcode.php
@@ -20,14 +20,12 @@ function tis_growth_stocks_shortcode() {
         'Motley Fool' => 'https://www.fool.com/investing/stock-market/types-of-stocks/growth-stocks/'
     ];
 
-    $api_key = get_option('tis_alpha_vantage_api_key');
-
     foreach ($results as $row) {
         if (is_numeric($row->score)) {
             $ticker = $row->ticker;
-            $company_info = tis_get_company_info($ticker, $api_key);
-            $company_name = $company_info['name'];
-            $market_price = $company_info['price'];
+            $company_name = $row->company_name;
+            $market_price = $row->cached_price;
+            $price_timestamp = $row->price_timestamp;
 
             $ticker_url = "https://inspireinsight.com/{$ticker}/US";
             $source_link = isset($source_links[$row->source]) ? $source_links[$row->source] : '#';
@@ -36,7 +34,7 @@ function tis_growth_stocks_shortcode() {
             $output .= "<td>{$ticker}</td>";
             $output .= "<td>{$company_name}</td>";
             $output .= "<td><a href='{$ticker_url}' target='_blank'>{$row->score}</a></td>";
-            $output .= "<td>{$market_price}</td>";
+            $output .= "<td>{$market_price} as of " . date('F j, g:i a', strtotime($price_timestamp)) . "</td>";
             $output .= "<td><a href='{$source_link}' target='_blank'>{$row->source}</a></td>";
             $output .= '</tr>';
         }
@@ -45,37 +43,6 @@ function tis_growth_stocks_shortcode() {
     $output .= '</tbody></table>';
 
     return $output;
-}
-
-function tis_get_company_info($ticker, $api_key) {
-    $company_name = 'N/A';
-    $market_price = 'N/A';
-
-    // Fetch company name
-    $name_url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol={$ticker}&apikey={$api_key}";
-    $name_response = file_get_contents($name_url);
-    if ($name_response) {
-        $name_data = json_decode($name_response, true);
-        if (isset($name_data['Name'])) {
-            $company_name = $name_data['Name'];
-        }
-    }
-
-    // Fetch market price
-    $price_url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={$ticker}&apikey={$api_key}";
-    $price_response = file_get_contents($price_url);
-    if ($price_response) {
-        $price_data = json_decode($price_response, true);
-        if (isset($price_data['Global Quote']['05. price'])) {
-            $market_price = number_format((float)$price_data['Global Quote']['05. price'], 2, '.', '');
-            $market_price = '$' . $market_price;
-        }
-    }
-
-    return [
-        'name' => $company_name,
-        'price' => $market_price
-    ];
 }
 
 add_shortcode('growth_stocks', 'tis_growth_stocks_shortcode');
